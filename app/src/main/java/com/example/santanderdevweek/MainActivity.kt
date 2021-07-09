8b3fdc8178ba0dbf269c7005b3af9d513e569a4a
Railson Rames Sousa <railson@msn.com>
2021-07-09 15:11:05 -0300

    Interface and some shapes to be more close to Santander App

---
+++ app/src/main/java/com/example/santanderdevweek/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.santanderdevweek
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflate = menuInflater
+        inflate.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.item_1 -> {
+                Log.d("Click", "Click no item1")
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
